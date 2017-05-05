e979f2d41bd0be4a15c7b6acf76d05152f5cdda9
Alice Lai <aplai168@gmail.com>
2017-05-05 14:58:35 -0500

    (feature) add search

--- client/app.js
+++ client/app.js
@@ -1,17 +1,28 @@
 const app = angular.module('reddit', ['ui.router']);
 
 
-app.controller('MainController', function($scope, $http) {
-    $scope.search = {};
-    // $http.get("http://www.reddit.com/search.json?q=ferrari")
-    $http.get('http://www.reddit.com/search.json?q=ferrari&sort=new')
-    // $http.get(`http://www.reddit.com/search.json?q=${topic}&sort=new`)
-
-    .success(function(response) {
-        // $scope.names = response.data.children;
-        $scope.posts = response.data.children;
-
-        console.log(response, 'response')
-        $scope.list = ['hey', 'whatupd'];
+app.controller('MainController', function ($scope, Subreddit) {
+  $scope.submit = (subreddit) => {
+    Subreddit.getTopic(subreddit)
+    .success((res) => {
+      $scope.posts = res.data.children;
     });
+  };
 });
+
+app.factory('Subreddit', function ($http) {
+  // $http.get("http://www.reddit.com/search.json?q=ferrari")
+  // $http.get('http://www.reddit.com/search.json?q=ferrari&sort=new')
+  return {
+    getTopic: topic => $http.get(`http://www.reddit.com/search.json?q=${topic}`),
+    // getTopic: topic => $http.get('http://www.reddit.com/search.json?q=ferrari&sort=new'),
+  }
+});
+
+// const getTopic = (topic) => {
+//   $http.get(`http://www.reddit.com/search.json?q=${topic}`)
+//   .success((response) => {
+//     $scope.posts = response.data.children;
+//     console.log(response, 'response')
+//   });
+// };
